aea350852c96ea97a5cdf4d55651c36230071650
MarceloSmg <celomsg@hotmail.com>
2024-08-25 20:18:05 -0300

    Adiciona suporte para Google Chrome no Streamlit Cloud

--- wz.py
+++ wz.py
@@ -163,10 +163,16 @@ def iniciar_whatsapp_web():
     global driver
     st.write("Iniciando o WhatsApp Web...")
 
+    # Configurações do Chrome para rodar em modo "headless"
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    
+    # Define explicitamente o caminho para o Chrome instalado
+    chrome_options.binary_location = "/usr/bin/google-chrome-stable"
 
     try:
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
